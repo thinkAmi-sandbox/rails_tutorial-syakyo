7826d5279819be661545ce8a00e3739773060cca
thinkAmi <dev.thinkami@gmail.com>
2014-01-13 21:56:35 +0000

    3.5.2

--- rails_projects/sample_app/spec/requests/static_pages_spec.rb
+++ rails_projects/sample_app/spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the context 'Sapmle App'" do
       visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -22,7 +24,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -35,7 +37,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -43,7 +45,7 @@ describe "Static pages" do
   describe "Contact page" do
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
 
     it "should have the content 'About Us'" do
