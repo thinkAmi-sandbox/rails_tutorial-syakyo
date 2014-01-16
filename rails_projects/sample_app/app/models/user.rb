ec44e00ca0d959e6cf94e9d98bf5b778d46043db
thinkAmi <dev.thinkami@gmail.com>
2014-01-16 07:27:00 +0000

    chaap6 finished

---
+++ rails_projects/sample_app/app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  before_save { email.downcase! }
+  
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  
+  validates :password, length: { minimum: 6 }
+end
