729e1cd5cde6557310f9f0c1c90b4455471ed9cd
thinkAmi <dev.thinkami@gmail.com>
2014-01-16 21:17:01 +0000

    7.4.1

--- rails_projects/sample_app/app/controllers/users_controller.rb
+++ rails_projects/sample_app/app/controllers/users_controller.rb
@@ -8,11 +8,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-
+      redirect_to @user
     else
       render 'new'
     end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
