24b74dd286e4ee3515ab9c28138635588aeefb4f
thinkAmi <dev.thinkami@gmail.com>
2014-01-18 21:37:01 +0000

    8.2.1

--- rails_projects/sample_app/app/models/user.rb
+++ rails_projects/sample_app/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
   before_save { email.downcase! }
+  before_create :create_remember_token
   
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -11,4 +13,19 @@ class User < ActiveRecord::Base
 
   
   validates :password, length: { minimum: 6 }
+
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
