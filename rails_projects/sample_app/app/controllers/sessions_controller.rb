24545a1d2eb1d4ca6fb5d7dd513e62c1926c9e0d
thinkAmi <dev.thinkami@gmail.com>
2014-01-18 23:02:07 +0000

    list 8.24

--- rails_projects/sample_app/app/controllers/sessions_controller.rb
+++ rails_projects/sample_app/app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      sign_in = user
+      sign_in user
       redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination'
