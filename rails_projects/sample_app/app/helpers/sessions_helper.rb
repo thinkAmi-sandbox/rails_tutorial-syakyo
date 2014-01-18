1bf8f8f55b88f2a87283f8f1faac96ef8ca16b9a
thinkAmi <dev.thinkami@gmail.com>
2014-01-18 23:16:32 +0000

    list 8.30

--- rails_projects/sample_app/app/helpers/sessions_helper.rb
+++ rails_projects/sample_app/app/helpers/sessions_helper.rb
@@ -19,4 +19,9 @@ module SessionsHelper
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
 end
