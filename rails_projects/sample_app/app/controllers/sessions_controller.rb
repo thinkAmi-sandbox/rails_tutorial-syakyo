fb8f6cf6e0b4325539c06f199fe1ada8c9ba7adb
thinkAmi <dev.thinkami@gmail.com>
2014-01-18 21:01:51 +0000

    list 8.12

--- rails_projects/sample_app/app/controllers/sessions_controller.rb
+++ rails_projects/sample_app/app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
--- rails_projects/sample_app/spec/requests/authentication_pages_spec.rb
+++ rails_projects/sample_app/spec/requests/authentication_pages_spec.rb
@@ -10,7 +10,7 @@ describe "Authentication" do
     it { should have_title('Sign in') }
   end
 
-  describe "sign in" do
+  describe "signin" do
     before { visit signin_path }
 
     describe "with invalid information" do
@@ -18,6 +18,11 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home"}
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
 
     describe "with valid information" do
