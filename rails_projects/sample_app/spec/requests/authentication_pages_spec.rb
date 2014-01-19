3d0035c2fc2e3630be6e13203d1fb270ad06413b
thinkAmi <dev.thinkami@gmail.com>
2014-01-19 07:46:26 +0000

    8.5.2-before

--- rails_projects/sample_app/spec/requests/authentication_pages_spec.rb
+++ rails_projects/sample_app/spec/requests/authentication_pages_spec.rb
@@ -16,7 +16,7 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click_button "Sign in" }
 
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_title('Sign in') }
       it { should have_error_message('Invalid') }
 
       describe "after visiting another page" do
@@ -28,9 +28,7 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        valid_signin user
       end
 
       it { should have_title(user.name) }
