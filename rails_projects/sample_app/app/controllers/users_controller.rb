87f73ccb5b18f66e4cdb83aa86944c55c6f827b8
thinkAmi <dev.thinkami@gmail.com>
2014-01-21 20:43:14 +0000

    9.6 finished

--- rails_projects/sample_app/app/controllers/users_controller.rb
+++ rails_projects/sample_app/app/controllers/users_controller.rb
@@ -12,17 +12,25 @@ class UsersController < ApplicationController
   end
   
   def new
-    @user = User.new
+    if signed_in?
+      redirect_to(root_path)
+    else
+      @user = User.new
+    end
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    if signed_in?
+      redirect_to(root_path)
     else
-      render 'new'
+      @user = User.new(user_params)
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
   end
 
@@ -39,9 +47,14 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
+    user = User.find(params[:id])
+    if current_user? user
+      redirect_to(root_path)
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_url
+    end
   end
 
   private
--- rails_projects/sample_app/spec/requests/authentication_pages_spec.rb
+++ rails_projects/sample_app/spec/requests/authentication_pages_spec.rb
@@ -64,6 +64,21 @@ describe "Authentication" do
         describe "after signing in" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+            expect(page).not_to have_title(user.name)
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default(profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
@@ -112,6 +127,18 @@ describe "Authentication" do
 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+    end
+
+    describe "as an admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before do
+        sign_in admin, no_capybara: true
+      end
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(admin) }
         specify { expect(response).to redirect_to(root_path) }
       end
     end
