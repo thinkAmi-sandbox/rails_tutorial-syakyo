8ade2e815928eb32de98bd46cc8779111a3749e2
thinkAmi <dev.thinkami@gmail.com>
2014-01-21 08:20:28 +0000

    9.5

--- rails_projects/sample_app/app/controllers/users_controller.rb
+++ rails_projects/sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -53,5 +60,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
--- rails_projects/sample_app/lib/tasks/sample_data.rake
+++ rails_projects/sample_app/lib/tasks/sample_data.rake
@@ -1,10 +1,11 @@
 namespace :db do
   desc "Fill database with sample data"
   task populate: :environment do
-    User.create!(name: "Example User",
-                 email: "example@railstutorial.jp",
-                 password: "foobar",
-                 password_confirmation: "foobar")
+    admin = User.create!(name: "Example User",
+                         email: "example@railstutorial.jp",
+                         password: "foobar",
+                         password_confirmation: "foobar",
+                         admin: true)
     99.times do |n|
       name = Faker::Name.name
       email = "example-#{n+1}@railstutorial.jp"
